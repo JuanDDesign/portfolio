7044feb967dc97486d9783535193d32187f99c3a
Juan Riaño <juand.design@gmail.com>
2025-01-18 21:26:42 -0500

    add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: Add domain site
-  site: "http:localhost:4321",
+  site: "https://juanda-portfolio.pages.dev",
   experimental: { svg: { mode: "sprite" } },
   integrations: [tailwind(), robotsTxt(), mdx()],
   i18n: { defaultLocale: "en", locales: ["en", "es"] },
